6cfb06492123fdbed7e574b1707cb393e4dd2fe5
dcolomer <colomertrave@gmail.com>
2025-03-27 19:00:11 +0100

    Delete comportamiento/15-Chain of Responsibility directory

--- comportamiento/15-Chain of Responsibility/src/logfrwk/core/CtesLog.java
+++
@@ -1,6 +0,0 @@
-package comportamiento.CoR.logfrwk.core;
-
-public enum CtesLog {
-	TRAZA, DEBUG, INFO, AVISO, ERROR, FATAL, // niveles de log
-	PANTALLA, FICHERO, PANTALLA_Y_FICHERO;	 // tipos de salida 		    
-}
