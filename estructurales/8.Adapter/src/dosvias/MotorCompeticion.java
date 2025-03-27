98f70eadeddf473fc0a27e4e0bac9d2d4adf4f60
dcolomer <colomertrave@gmail.com>
2025-03-27 15:54:57 +0100

    Add files via upload

---
+++ estructurales/8.Adapter/src/dosvias/MotorCompeticion.java
@@ -0,0 +1,24 @@
+package estructurales.adapter.dosvias;
+
+public class MotorCompeticion implements IMotorGasolina {
+
+	public MotorCompeticion(){
+        System.out.println("Creando una instancia de motor competicion");
+    }
+	
+	@Override
+	public void encender() {
+		System.out.println("motor competicion encendido");
+	}
+
+	@Override
+	public void acelerar() {
+		System.out.println("motor competicion acelerado");
+	}
+
+	@Override
+	public void apagar() {
+		System.out.println("motor competicion apagado");
+	}
+
+}
