bce95b4e0286a8a9b3c9aad064d04968d0164ae2
dcolomer <colomertrave@gmail.com>
2025-03-27 15:51:54 +0100

    Add files via upload

---
+++ estructurales/8.Adapter/src/dosvias/IMotorElectrico.java
@@ -0,0 +1,9 @@
+package estructurales.adapter.dosvias;
+
+public interface IMotorElectrico {
+	public void conectar();
+	public void activar();
+	public void aumentarVelocidad();
+	public void detener();
+	public void desconectar();
+}
---
+++ estructurales/8.Adapter/src/dosvias/MotorElectricoImpl.java
@@ -0,0 +1,51 @@
+package estructurales.adapter.dosvias;
+
+public class MotorElectricoImpl implements IMotorElectrico {
+
+	private boolean conectado = false;
+
+	public MotorElectricoImpl() {
+		System.out.println("Creando instancia de motor electrico");
+	}
+
+	@Override
+	public void conectar() {
+		System.out.println("motor electrico conectado");
+		this.conectado = true;
+	}
+
+	@Override
+	public void activar() {
+		if (!conectado) {
+            System.out
+            	.println("No se puede activar porque no esta conectado el motor electrico");
+        } else {
+            System.out.println("motor electrico activado");
+        }
+	}
+
+	@Override
+	public void aumentarVelocidad() {
+		if (!conectado) {
+            System.out.println("No se puede acelerar el motor electrico porque no esta conectado");
+        } else {
+            System.out.println("motor electrico acelerado");
+        }
+	}
+
+	@Override
+	public void detener() {
+		if (!conectado) {
+            System.out.println("No se puede detener motor electrico porque no esta conectado");
+        } else {
+            System.out.println("motor electrico detenido");
+        }
+	}
+
+	@Override
+	public void desconectar() {
+		System.out.println("motor electrico desconectado");
+        conectado = false;
+	}
+
+}
