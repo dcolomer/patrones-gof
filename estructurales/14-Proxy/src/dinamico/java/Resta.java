75afc53cb4c01b30e884c2393db5e3f349625053
dcolomer <colomertrave@gmail.com>
2025-03-27 18:40:00 +0100

    Delete estructurales/14-Proxy directory

--- estructurales/14-Proxy/src/dinamico/java/Resta.java
+++
@@ -1,7 +0,0 @@
-package estructurales.proxy.dinamico.java;
-/*
- * Interfaz que tiene que implementar un RealSubject
- */
-public interface Resta {
-	public int restar(int a, int b);
-}
--- estructurales/14-Proxy/src/dinamico/java/Suma.java
+++
@@ -1,8 +0,0 @@
-package estructurales.proxy.dinamico.java;
-/*
- * Interfaz que tiene que implementar el RealSubject
- */
-public interface Suma {
-	public int sumar(int a, int b);
-	public int sumarN(int a, int... b);
-}
