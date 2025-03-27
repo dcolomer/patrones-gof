71cc403046a564cf92dd776c2d49dec748b7141f
dcolomer <colomertrave@gmail.com>
2025-03-27 15:57:31 +0100

    Delete estructurales/8.Adapter directory

--- estructurales/8.Adapter/src/MotorElectrico.java
+++
@@ -1,9 +0,0 @@
-package estructurales.adapter;
-
-public interface MotorElectrico {
-	public void conectar();
-	public void activar();
-	public void aumentarVelocidad();
-	public void detener();
-	public void desconectar();
-}
