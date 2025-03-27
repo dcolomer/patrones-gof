7a5b4e666ee605c5e0dd166ec874fbc42564ea0d
dcolomer <colomertrave@gmail.com>
2025-03-27 15:48:46 +0100

    Add files via upload

---
+++ 2-estructurales/8.Adapter/src/Motor.java
@@ -0,0 +1,8 @@
+package estructurales.adapter;
+
+public abstract class Motor {
+	abstract public void encender();
+	abstract public void acelerar();
+	abstract public void apagar();
+	// ...mas metodos, con codigo para heredar muy interesante
+}
