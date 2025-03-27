a3c953ffbf38f31a608180d9bba9e297dd023a7e
dcolomer <colomertrave@gmail.com>
2025-03-27 18:39:37 +0100

    Delete estructurales/12-Facade directory

--- estructurales/12-Facade/src/subsistema/TarjetaTipo.java
+++
@@ -1,8 +0,0 @@
-package estructurales.facade.subsistema;
-
-public enum TarjetaTipo {
-	VISA, DISCOVER, MASTER;
-	public static final int LENGHT_VISA = 14;
-	public static final int LENGHT_DISCOVER = 15;
-	public static final int LENGHT_MASTER = 16;	
-}
