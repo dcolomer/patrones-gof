da37225d49ff92af4f4908bae39c90c57f677ffe
dcolomer <colomertrave@gmail.com>
2025-03-27 15:52:50 +0100

    Delete estructurales/8.Adapter/src directory

--- estructurales/8.Adapter/src/dosvias/IMotorElectrico.java
+++
@@ -1,9 +0,0 @@
-package estructurales.adapter.dosvias;
-
-public interface IMotorElectrico {
-	public void conectar();
-	public void activar();
-	public void aumentarVelocidad();
-	public void detener();
-	public void desconectar();
-}
--- estructurales/8.Adapter/src/dosvias/IMotorGasolina.java
+++
@@ -1,7 +0,0 @@
-package estructurales.adapter.dosvias;
-
-public interface IMotorGasolina {
-	abstract public void encender();
-	abstract public void acelerar();
-	abstract public void apagar();	
-}
